ee10dc09754be0851fdfa2e1a1115c4213b02924
Cendy Saputra <cendysaputra04@gmail.com>
2025-11-21 09:03:04 +0700

    Update Overloading

--- Forum Session 2/overloading.java
+++ Forum Session 2/overloading.java
@@ -1,3 +1,22 @@
-public class overloading {
+class Hitung {
+  int kali(int a, int b) {
+    return a * b;
+  }
 
+  int kali(int a, int b, int c) {
+    return a * b * c;
+  }
+
+  double kali(double a, double b) {
+    return a * b;
+  }
+}
+
+public class overloading {
+  public static void main(String[] args) {
+    Hitung hit = new Hitung();
+    System.out.println("2 x 3 = " + hit.kali(2, 3));
+    System.out.println("2 x 3 x 4 = " + hit.kali(2, 3, 4));
+    System.out.println("2.5 x 4.0 = " + hit.kali(2.5, 4.0));
+  }
 }
