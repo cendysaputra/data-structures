f41967773e5ad12c7273432be063770f821d1b0d
Cendy Saputra <cendysaputra04@gmail.com>
2026-01-15 20:05:25 +0700

    Add QueueUsingStackMain

---
+++ Practicum Module 3 - Stack & Queue/QueueUsingStackMain.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class QueueUsingStack {
+   Stack<Integer> stack1 = new Stack<>();
+   Stack<Integer> stack2 = new Stack<>();
+
+   void enqueue(int data) {
+      stack1.push(data);
+   }
+
+   int dequeue() {
+      if (stack2.isEmpty()) {
+         if (stack1.isEmpty()) {
+            System.out.println("Queue is Empty");
+            return -1;
+         }
+         while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+         }
+      }
+      return stack2.pop();
+   }
+}
+
+public class QueueUsingStackMain {
+   public static void main(String[] args) {
+      QueueUsingStack queue = new QueueUsingStack();
+      queue.enqueue(10);
+      queue.enqueue(20);
+      queue.enqueue(30);
+
+      System.out.println("Elemen yang di-dequeue: " + queue.dequeue());
+      System.out.println("Elemen yang di-dequeue: " + queue.dequeue());
+      System.out.println("Elemen yang di-dequeue: " + queue.dequeue());
+   }
+}
